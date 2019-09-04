6615d594e37ee18ad7a8b83783e56c69df4fa979
mmy <mmy_xize@163.com>
2019-09-04 20:44:34 +0800

    安装vue-cli@3对应的element插件

--- vue-router-todolist/src/App.vue
+++ vue-router-todolist/src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
+    HelloWorld
   }
 }
 </script>
