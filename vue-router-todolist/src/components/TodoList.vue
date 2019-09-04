607f7b53b61aaad8bb1943be733d19d58a66222d
mmy <mmy_xize@163.com>
2019-09-04 20:22:44 +0800

    添加欢迎页面、首页、我的、todolist的路由

--- vue-router-todolist/src/components/TodoList.vue
+++ vue-router-todolist/src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="todolist">
-      <h1>首页</h1>
+      <h1>todolist</h1>
   </div>
 </template>
 
---
+++ vue-router-todolist/src/router/router.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import App from '../App.vue'
+import Home from '../components/Home.vue'
+import Person from '../components/Person.vue'
+import TodoList from '../components/TodoList.vue'
+
+const router = new VueRouter({
+    routes: [
+        { path: '/', component: App },
+        { path: '/home', component: Home },
+        { path: '/person', component: Person },
+        { path: '/todolist', component: TodoList }
+    ]
+});
+
+export default router;
