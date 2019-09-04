3f7b53d89add991a2dcf9a332c94fab74f4bf88a
mmy <mmy_xize@163.com>
2019-09-04 21:38:46 +0800

    添加欢迎页面输入框

--- vue-router-todolist/src/components/Main.vue
+++ vue-router-todolist/src/components/Main.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="main">
     <h1>欢迎页面</h1>
+    <input placeholder="请输入用户名" v-model="input" id="input-name" />
     <el-button @click="beginApp">开始使用</el-button>
   </div>
 </template>
@@ -10,6 +11,11 @@ export default {
   name: "Main",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      input: ""
+    };
   },
   methods: {
     beginApp: function() {
@@ -21,4 +27,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#input-name {
+  width: 300px;
+  height: 30px;
+  padding: 4px;
+  margin: 20px;
+}
 </style>
