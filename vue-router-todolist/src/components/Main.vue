4034fcb90f0d3af08f2a0fa37d6183b35db0d603
mmy <mmy_xize@163.com>
2019-09-04 20:32:29 +0800

    添加欢迎页面，注册路由，并实现欢迎页面跳转至首页

---
+++ vue-router-todolist/src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+    <h1>欢迎页面</h1>
+    <button @click="beginApp">开始使用</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: {
+    msg: String
+  },
+  methods: {
+    beginApp: function() {
+      this.$router.push("home");
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- vue-router-todolist/src/router/router.js
+++ vue-router-todolist/src/router/router.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 import App from '../App.vue'
+import Main from '../components/Main.vue'
 import Home from '../components/Home.vue'
 import Person from '../components/Person.vue'
 import TodoList from '../components/TodoList.vue'
@@ -11,6 +12,7 @@ import TodoList from '../components/TodoList.vue'
 const router = new VueRouter({
     routes: [
         { path: '/', component: App },
+        { path: '/main', component: Main },
         { path: '/home', component: Home },
         { path: '/person', component: Person },
         { path: '/todolist', component: TodoList }
