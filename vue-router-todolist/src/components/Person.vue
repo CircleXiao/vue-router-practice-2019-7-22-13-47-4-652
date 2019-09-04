669587480dff8ec9767297b875d359e0036b3082
mmy <mmy_xize@163.com>
2019-09-04 20:12:56 +0800

    创建首页、我的、todolist组件

---
+++ vue-router-todolist/src/components/Person.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="person">
+      <h1>我的</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Person',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
